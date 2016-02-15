60985db2c83159111ff879f9e958023ebf1c1b2c
Cyrille M <cmeichel@free.fr>
2016-02-15 23:37:30 +0100

    bug fix on redirection

--- src/ctrl/get-login-redirection.js
+++ src/ctrl/get-login-redirection.js
@@ -4,11 +4,14 @@ module.exports = function(server) {
     return function (req, res, next) {
         req.session.redirection = server.config.doc.redirection;
         var appCtrl = server.controllers.application;
-        if (req.body.appId) {
-            appCtrl.readApplicationById(req.body.appId, function(err, data) {
+        if (req.query.appId) {
+            server.console.log('Getting application', req.query.appId);
+            appCtrl.readApplicationById(req.query.appId, function(err, data) {
                 if ((!err) && (data)) {
                     server.console.log('Application found', data.name);
                     req.session.redirection = data.redirection;
+                } else {
+                    server.console.log('Application Not found');
                 }
                 return next();
             });
